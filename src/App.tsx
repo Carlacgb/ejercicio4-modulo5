d90df73a34e4cd685a965734b6eba28b8e0828c3
Caita <carlacgb@gmail.com>
2025-04-18 10:51:07 -0400

    actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import Button from './components/Button';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Aprendizaje Basado en Proyectos: Mejorando la
-      Web del Hospital con APIs, TypeScript, y Seguridad </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const handleClick = () => {
+alert('Botón presionado');
+};
+return (
+<div>
+<h1>Mejorando la
+Web del Hospital con APIs, TypeScript, y Seguridad</h1>
+<Button label="Haz clic aquí" onClick={handleClick} />
+</div>
+);
 }
-
-export default App
+export default App;
